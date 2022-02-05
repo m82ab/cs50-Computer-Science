a7b346ae40fe1f5547e5cb6e76d1fc6f130b27e1
Mohammad Mahdi Abadeh <67842065+m82ab@users.noreply.github.com>
2022-02-05 10:24:32 +0330

    Delete pset6 directory

--- pset6/hello/hello.py
+++
@@ -1,6 +0,0 @@
-from cs50 import get_string
-
-# Question from the user and get the answer
-answer = get_string("What is your name?")
-# The output is first heloo and then the text that the user enters
-print("hello, " + answer)
